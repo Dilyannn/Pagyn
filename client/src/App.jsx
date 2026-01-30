1bf4295bded6ebb070d08e4a3bf8f8a55bcfd27d
Dilyannn <dilyanpaypal@gmail.com>
2026-01-30 14:41:59 +0200

    Add protected routes and toast notifications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,10 @@ import ProtectedRoute from './components/auth/ProtectedRoute'
 import LandingPage from './pages/LandingPage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
 import RegisterPage from './pages/RegisterPage.jsx'
+import DashboardPage from './pages/DashboardPage.jsx'
+import EditorPage from './pages/EditorPage.jsx'
+import ViewBookPage from './pages/ViewBookPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
 
 
 function App() {
@@ -14,6 +18,23 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+
+        <Route 
+          path="/dashboard" 
+          element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} 
+        />
+        <Route 
+          path="/editor/:bookId" 
+          element={<ProtectedRoute><EditorPage /></ProtectedRoute>} 
+        />
+        <Route 
+          path="/view-book/:bookId" 
+          element={<ProtectedRoute><ViewBookPage /></ProtectedRoute>} 
+        />
+        <Route 
+          path="/profile" 
+          element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} 
+        />
       </Routes>
     </>
   )
