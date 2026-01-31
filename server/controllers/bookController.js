d9a8d9a8f7dbda19a9393dbd63ae5a9038527e66
Dilyannn <dilyanpaypal@gmail.com>
2026-01-31 13:29:17 +0200

    Return user-specific books in getBooks controller

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -34,7 +34,8 @@ const createBook = async (req, res) => {
  */
 const getBooks = async (req, res) => {
   try {
-
+    const books = await Book.find({ userId: req.user._id }).sort({ createdAt: -1 }); // Latest books first
+    res.status(200).json(books);
   } catch (err) {
     res.status(500).json({ message: 'Server error', details: err.message });
   }
