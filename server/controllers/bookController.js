82eeb5a6a0e6e11a5ec9ebe0630bc56a554b0fd7
Dilyannn <dilyanpaypal@gmail.com>
2026-01-31 13:30:57 +0200

    Add authorization checks to getBooksById controller

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -48,7 +48,17 @@ const getBooks = async (req, res) => {
  */
 const getBooksById = async (req, res) => {
   try {
-
+    const book = await Book.findById(req.params.id);
+    
+    if (!book) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    
+    if (book.userId.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: 'Unauthorized access' });
+    }
+    
+    res.status(200).json(book);
   } catch (err) {
     res.status(500).json({ message: 'Server error', details: err.message });
   }
