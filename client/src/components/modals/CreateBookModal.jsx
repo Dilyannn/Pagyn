d4af51bc573347c201b63d141a3ecdc6aeafcf71
Dilyannn <dilyanpaypal@gmail.com>
2026-02-06 14:11:39 +0200

    create book modal initial structure (logic only)
    finalize the initial struture of the modal for creating books, still need to add the UI and styles

--- client/src/components/modals/CreateBookModal.jsx
+++ client/src/components/modals/CreateBookModal.jsx
@@ -1,8 +1,89 @@
-import React from 'react'
+import { useState, useRef, useEffect } from "react";
+import axiosInstance from "../../utils/axiosInstance.js";
+import { API_ENDPOINTS } from "../../utils/api.js";
+import { useAuth } from "../../context/AuthContext.js";
 
-function CreateBookModal() {
+import Modal from "../ui/Modal";
+import InputField from "../ui/InputField";
+import Button from "../ui/Button";
+import SelectField from "../ui/SelectField";
+
+import {
+  Plus,
+  Sparkles,
+  Trash2,
+  ArrowLeft,
+  BookOpen,
+  Hash,
+  LightBulb,
+  Palette,
+} from "lucide-react";
+import toast from "react-hot-toast";
+
+function CreateBookModal({ isOpen, onClose, onBookCreated }) {
+  const { user } = useAuth();
+
+  const [step, setStep] = useState(1);
+  const [title, setTitle] = useState("");
+  const [numChapters, setNumChapters] = useState(3);
+
+  const [topic, setTopic] = useState(""); // for the AI outline generation
+  const [style, setStyle] = useState(""); // for the AI outline generation
+  const [chapters, setChapters] = useState([]); // AI-generated chapters
+  const [isGeneratingOutline, setIsGeneratingOutline] = useState(false); 
+  const [isFinalizing, setIsFinalizing] = useState(false);
+  
+  const chaptersRef = useRef([]); //& chapter container ref for auto-scrolling
+
+  const resetModal = () => {
+    setStep(1);
+    setTitle("");
+    setNumChapters(3);
+    setTopic("");
+    setStyle("");
+    setChapters([]);
+    setIsGeneratingOutline(false);
+    setIsFinalizing(false);
+  };
+
+  const handleGenerateOutline = async () => {};
+
+  const handleChapterChange = (index, field, value) => {
+    const updatedChapters = [...chapters];
+    updatedChapters[index][field] = value;
+    setChapters(updatedChapters);
+  };
+
+  const handleChapter = () => {
+    setChapters([
+      ...chapters,
+      { title: `Chapter ${chapters.length + 1}`, description: "" },
+    ]);
+  };
+
+  const handleFinalizeBook = async () => {};
+
+  useEffect(() => {
+    if (step === 2 && chaptersRef.current) {
+      const scrollableDiv = chaptersRef.current;
+      scrollableDiv.scrollTo({
+        top: scrollableDiv.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }), [chapters.length, step];
+
   return (
-    <div>CreateBookModal</div>
+    <Modal
+      isOpen={isOpen}
+      onClose={() => {
+        onClose();
+        resetModal();
+      }}
+      title="Create New eBook"
+    >
+      Content Here
+    </Modal>
   )
 }
 
